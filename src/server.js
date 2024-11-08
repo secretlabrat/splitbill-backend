95b9ca85aa40e61442e13a30e5a3b20b0e6893ba
secretlabrat <secretlaboratory333@gmail.com>
2024-11-08 11:57:07 -0800

    fix cors and add more example

--- src/server.js
+++ src/server.js
@@ -5,7 +5,15 @@ const port = process.env.SERVERPORT || 3000;
 const apiRoute = require("./api");
 
 app.use(
-  cors({ origin: ["https://splitbill.kidkrub.me/", "http://localhost"] })
+  cors({
+    origin: [
+      "https://splitbill.kidkrub.me/",
+      "http://127.0.0.1:3000",
+      "http://localhost:3000",
+      "http://localhost:8080",
+      "http://127.0.0.1:8080",
+    ],
+  })
 );
 
 app.use(express.json());
