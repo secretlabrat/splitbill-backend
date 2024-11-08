fff1d6cff1a87130f83efb4ffd2197eea51dbb27
secretlabrat <secretlaboratory333@gmail.com>
2024-11-08 11:43:13 -0800

    install firebase admins sdk enable cors and add example for history API

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const healthcheckHandler = require("./healthcheck");
+const historyHandler = require("./history");
 
 router.use("/healthcheck", healthcheckHandler);
+router.use("/history", historyHandler);
 
 module.exports = router;
